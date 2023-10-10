2b8afcc3f848be4d9d145fb8afa8f7f120659b1d
Kunal <jainkunal129@gmail.com>
2023-10-10 13:14:23 +0530

    web version

--- lib/pages/attendance_calc.dart
+++ lib/pages/attendance_calc.dart
@@ -24,28 +24,24 @@ class CalculatorState extends State<Calculator> {
         backgroundColor: const Color(0xff198754),
       ),
       body: Center(
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Text(
-                'Attendance Calculator',
-                style: GoogleFonts.roboto(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 50,
-                ),
-                textAlign: TextAlign.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Attendance Calculator',
+              style: GoogleFonts.roboto(
+                fontWeight: FontWeight.bold,
+                fontSize: 50,
               ),
-              const SizedBox(
-                height: 20,
-              ),
-              const PasteButton(),
-              const SizedBox(
-                height: 30,
-              ),
-              Container(
+              textAlign: TextAlign.center,
+            ),
+            const PasteButton(),
+            Expanded(
+              child: Container(
                 margin: const EdgeInsets.all(20),
                 decoration: BoxDecoration(
                   color: Colors.white,
@@ -56,11 +52,12 @@ class CalculatorState extends State<Calculator> {
                   borderRadius: BorderRadius.circular(20),
                 ),
                 child: ClipRRect(
-                    borderRadius: BorderRadius.circular(20),
-                    child: Image.asset('assets/tutorial/tutorial.jpeg')),
-              )
-            ],
-          ),
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image.asset('assets/tutorial/tutorial.jpeg'),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
